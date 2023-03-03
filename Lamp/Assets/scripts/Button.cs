543332bce079d7123c5f4e9d8e8a17dbd153433b
ToketsuKz <66639624+ToketsuKz@users.noreply.github.com>
2023-03-03 09:44:43 +0500

    C#-Lamp

---
+++ Lamp/Assets/scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public GameObject text;
+    int textNo;
+    private bool text_1 = false;
+
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            text_1=!text_1;
+            text.gameObject.SetActive(text_1);
+        }
+    }
+}
---
+++ Lamp/Assets/scripts/Lamp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lamp : MonoBehaviour {
+
+    public GameObject online;
+    public GameObject offline;
+    private bool online_1 = true;
+    private bool offline_1 = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            online_1 = !online_1;
+            online.gameObject.SetActive(online_1);
+        }
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            offline_1 = !offline_1;
+            offline.gameObject.SetActive(offline_1);
+        }
+    }
+}
